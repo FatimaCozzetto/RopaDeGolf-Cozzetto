5f0a6387c4bc12fa174ce3997a8ae524a52579f5
Fátima Cozzetto <86528825+FatimaCozzetto@users.noreply.github.com>
2021-10-16 13:01:49 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 /*CARRITO DE COMPRAS*/
-
 class Producto {
-    constructor (nombre, precio) {
-        this.nombre = nombre;
+    constructor (nombre, color, talle, precio) {
+        this.nombre = nombre.toLowerCase();
+        this.color = color.toLowerCase();
+        this.talle = talle.toLowerCase();
         this.precio = precio;
     }
 
@@ -10,25 +11,93 @@ class Producto {
         return this.precio;
     }
 
+    getColor(){
+        return this.color;
+    }
+
+    getTalle(){
+        return this.talle;
+    }
+
     getPrecioCon10PorcDeDescuento(){
         return this.precio * 0.90;
     }
 }
 
-//REMERA
-const producto1 = new producto('Remera', 1500);
+//LISTA DE PRODUCTOS 
+const productos = [];
 
+//RETORNA LISTA DE PRODUCTO
+const getAll = () => {
+    return productos;
+}
+
+//AGREGAR UN PRODUCTO A LA LISTA
+const creat = (productos) => {
+    productos.push(producto);
+}
+
+//HALLAR UN PRODUCTO POR EL NOMBRE
+const findOne = (nombre) => {
+    nombre = nombre.toLowerCase();
+    const producto = productos.find( producto => producto.nombre === nombre);
+    if (!producto){
+        throw new Error(`No existe ${nombre}`);
+    }
+    return producto;
+}
+
+//ELIMINAR PRODUCTOS
+const remove = (nombre) => {
+    const producto = findOne(nombre);
+    const index = productos.indexOf(producto);
+    productos.splice (index, 1)
+}
+
+//MODIFICAR UN PRODUCTO
+const update = (nombre, color) => {
+    const producto = findOne(nombre);
+    producto.color = color;
+}
+
+const update = (nombre, talle) => {
+    const producto = findOne(nombre);
+    producto.talle = talle;
+}
+
+const update = (nombre, precio) => {
+    const producto = findOne(nombre);
+    producto.precio = precio;
+}
+
+//REMERA
+const producto1 = new producto('Remera', 'Blanca', 'L' ,1500);
+
+console.log(producto1.getColor);
+console.log(producto1.getTalle);
 console.log(producto1.getPrecio);
 console.log(producto1.getPrecioCon10PorcDeDescuento());
 
 //CHOMBA
-const producto2 = new producto('Chomba', 1800);
+const producto2 = new producto('Chomba', 'Azul', 'M', 1800);
 
+console.log(producto2.getColor);
+console.log(producto2.getTalle);
 console.log(producto2.getPrecio);
 console.log(producto2.getPrecioCon10PorcDeDescuento());
 
 //PANTALÓN
-const producto3 = new producto('Pantalón', 3300);
+const producto3 = new producto('Pantalón', 'Nude', 'M', 3300);
 
+console.log(producto3.getColor);
+console.log(producto3.getTalle);
 console.log(producto3.getPrecio);
 console.log(producto3.getPrecioCon10PorcDeDescuento);
+
+//AGREGO PRODUCTO A LA LISTA
+creat(producto1);
+creat(producto2);
+creat(producto3);
+
+//OBTENER LISTA COMPLETA DE PRODUCTOS
+console.log(getAll());
